7116264c28d6f1e59a233f2c55f699f707eee4c4
Yukigeshiki <laurencesonnenberg01@gmail.com>
2020-03-05 15:35:21 +0200

    add footer

---
+++ scripts/footerHtml.js
@@ -0,0 +1,64 @@
+document.write(`
+    <footer>
+    <div class="row">
+      <div class="contact-us">
+        <h3>Contact us:</h3>
+        <p><strong>Tell: </strong> +27 00 000 0000</p>
+        <p><strong>Email: </strong> info@oddplantsucculents.com</p>
+        <p>
+          <strong>Address: </strong><br />
+          04 Oberon Way<br />
+          Meadowridge<br />
+          7806<br />
+          Cape Town<br />
+          South Africa<br />
+        </p>
+      </div>
+      <div class="follow-us">
+        <h3>Follow us on:</h3>
+        <div class="icons">
+          <a
+            href="https://www.instagram.com"
+            class="inline-link"
+            target="_blank"
+          >
+            <!-- credit to https://icons8.com/icon/85154/instagram for insta icon -->
+            <img
+              class="icon"
+              src="https://img.icons8.com/material-outlined/48/000000/instagram-new.png"
+            />
+          </a>
+          <a
+            href="https://www.facebook.com"
+            class="inline-link"
+            target="_blank"
+          >
+            <!-- credit to https://icons8.com/icon/118487/facebook for facebook icon -->
+            <img
+              class="icon"
+              src="https://img.icons8.com/material-outlined/48/000000/facebook-new.png"
+            />
+          </a>
+          <a
+            href="https://www.twitter.com"
+            class="inline-link"
+            target="_blank"
+          >
+            <!-- credit to https://icons8.com/icon/3861/twitter for twitter icon -->
+            <img
+              class="icon"
+              src="https://img.icons8.com/android/96/000000/twitter.png"
+            />
+          </a>
+        </div>
+      </div>
+      <div class="newsletter">
+        <h3>Signup for our monthly newsletter:</h3>
+        <form class="newsletter-form">
+          <input type="text" class="input" placeholder="Email Address" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+    <p class="copyright">&copy Copyright Odd Plant Succulents 2020</p>
+    </footer>`);

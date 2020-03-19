dbb324c577514d676490b3261c10ce6e4ee7cc01
Yukigeshiki <laurencesonnenberg01@gmail.com>
2020-03-19 16:16:24 +0200

    add lorem ipsum text to article body

--- scripts/articleData.js
+++ scripts/articleData.js
@@ -2,10 +2,74 @@
 articles = {
   articleOne: {
     title: 'Succulent Care Under the Hot African Sun',
-    body: "This is a article One's body"
+    body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Semper quis lectus nulla at volutpat diam ut. Augue ut lectus arcu bibendum at. Volutpat blandit aliquam etiam erat. 
+    Nec tincidunt praesent semper feugiat nibh sed pulvinar. Placerat vestibulum lectus mauris ultrices eros in. Consectetur purus ut faucibus pulvinar. 
+    Malesuada fames ac turpis egestas maecenas pharetra. Eget sit amet tellus cras. Amet luctus venenatis lectus magna fringilla urna. 
+    In fermentum posuere urna nec tincidunt. Ut tellus elementum sagittis vitae et leo duis. Lacus luctus accumsan tortor posuere ac.
+    Semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. Turpis in eu mi bibendum neque egestas congue. 
+    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. 
+    Eget sit amet tellus cras adipiscing. Vel facilisis volutpat est velit egestas. Aliquam id diam maecenas ultricies mi eget mauris pharetra. 
+    Accumsan in nisl nisi scelerisque. Lacus sed viverra tellus in. Sed turpis tincidunt id aliquet risus feugiat in ante metus. 
+    Aliquet lectus proin nibh nisl. Quis commodo odio aenean sed adipiscing diam donec adipiscing tristique. Aliquam ut porttitor leo a diam. 
+    Tristique risus nec feugiat in fermentum posuere. Egestas quis ipsum suspendisse ultrices.
+    Aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper. 
+    Sed vulputate mi sit amet mauris commodo quis. At urna condimentum mattis pellentesque id nibh tortor id aliquet. 
+    Consectetur adipiscing elit pellentesque habitant morbi tristique. Ornare quam viverra orci sagittis eu volutpat. 
+    Commodo nulla facilisi nullam vehicula ipsum. Scelerisque eu ultrices vitae auctor. Vel quam elementum pulvinar etiam non. 
+    Elit eget gravida cum sociis natoque penatibus et. Tincidunt tortor aliquam nulla facilisi cras.
+    Commodo ullamcorper a lacus vestibulum sed arcu non odio. Bibendum neque egestas congue quisque egestas diam in arcu.
+    Risus quis varius quam quisque id diam. Condimentum vitae sapien pellentesque habitant morbi. Maecenas ultricies mi eget mauris pharetra et. 
+    Maecenas pharetra convallis posuere morbi leo urna molestie. Viverra tellus in hac habitasse platea. 
+    Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Facilisis volutpat est velit egestas dui id. 
+    Varius sit amet mattis vulputate enim nulla. Elit scelerisque mauris pellentesque pulvinar. 
+    Vitae tortor condimentum lacinia quis vel eros donec. Vitae purus faucibus ornare suspendisse sed nisi lacus sed. 
+    Sagittis purus sit amet volutpat. Condimentum id venenatis a condimentum vitae sapien pellentesque habitant.
+    Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Commodo viverra maecenas accumsan lacus vel facilisis volutpat. 
+    Pharetra et ultrices neque ornare. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus. Lectus proin nibh nisl condimentum id venenatis.
+    Leo vel fringilla est ullamcorper. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. 
+    Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Id ornare arcu odio ut. 
+    Dolor sed viverra ipsum nunc aliquet bibendum enim. Convallis convallis tellus id interdum. Aliquam ultrices sagittis orci a scelerisque purus semper. 
+    Morbi tristique senectus et netus et malesuada fames ac turpis. Nam at lectus urna duis convallis convallis tellus id interdum. 
+    Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Urna molestie at elementum eu. Sit amet nulla facilisi morbi tempus iaculis urna id. 
+    Amet consectetur adipiscing elit pellentesque habitant. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis. \\n\\n
+    Lacus sed turpis tincidunt id aliquet risus feugiat in. Integer eget aliquet nibh praesent. Ac turpis egestas integer eget aliquet. 
+    Egestas diam in arcu cursus euismod quis viverra nibh.`
   },
   articleTwo: {
     title: 'Winter Succulents to Make the Best of the Cold Rainy Season',
-    body: "This is a article Two's body"
+    body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Semper quis lectus nulla at volutpat diam ut. Augue ut lectus arcu bibendum at. Volutpat blandit aliquam etiam erat. 
+    Nec tincidunt praesent semper feugiat nibh sed pulvinar. Placerat vestibulum lectus mauris ultrices eros in. Consectetur purus ut faucibus pulvinar. 
+    Malesuada fames ac turpis egestas maecenas pharetra. Eget sit amet tellus cras. Amet luctus venenatis lectus magna fringilla urna. 
+    In fermentum posuere urna nec tincidunt. Ut tellus elementum sagittis vitae et leo duis. Lacus luctus accumsan tortor posuere ac.
+    Semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. Turpis in eu mi bibendum neque egestas congue. 
+    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. 
+    Eget sit amet tellus cras adipiscing. Vel facilisis volutpat est velit egestas. Aliquam id diam maecenas ultricies mi eget mauris pharetra. 
+    Accumsan in nisl nisi scelerisque. Lacus sed viverra tellus in. Sed turpis tincidunt id aliquet risus feugiat in ante metus. 
+    Aliquet lectus proin nibh nisl. Quis commodo odio aenean sed adipiscing diam donec adipiscing tristique. Aliquam ut porttitor leo a diam. 
+    Tristique risus nec feugiat in fermentum posuere. Egestas quis ipsum suspendisse ultrices.
+    Aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper. 
+    Sed vulputate mi sit amet mauris commodo quis. At urna condimentum mattis pellentesque id nibh tortor id aliquet. 
+    Consectetur adipiscing elit pellentesque habitant morbi tristique. Ornare quam viverra orci sagittis eu volutpat. 
+    Commodo nulla facilisi nullam vehicula ipsum. Scelerisque eu ultrices vitae auctor. Vel quam elementum pulvinar etiam non. 
+    Elit eget gravida cum sociis natoque penatibus et. Tincidunt tortor aliquam nulla facilisi cras.
+    Commodo ullamcorper a lacus vestibulum sed arcu non odio. Bibendum neque egestas congue quisque egestas diam in arcu.
+    Risus quis varius quam quisque id diam. Condimentum vitae sapien pellentesque habitant morbi. Maecenas ultricies mi eget mauris pharetra et. 
+    Maecenas pharetra convallis posuere morbi leo urna molestie. Viverra tellus in hac habitasse platea. 
+    Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Facilisis volutpat est velit egestas dui id. 
+    Varius sit amet mattis vulputate enim nulla. Elit scelerisque mauris pellentesque pulvinar. 
+    Vitae tortor condimentum lacinia quis vel eros donec. Vitae purus faucibus ornare suspendisse sed nisi lacus sed. 
+    Sagittis purus sit amet volutpat. Condimentum id venenatis a condimentum vitae sapien pellentesque habitant.
+    Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Commodo viverra maecenas accumsan lacus vel facilisis volutpat. 
+    Pharetra et ultrices neque ornare. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus. Lectus proin nibh nisl condimentum id venenatis.
+    Leo vel fringilla est ullamcorper. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. 
+    Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Id ornare arcu odio ut. 
+    Dolor sed viverra ipsum nunc aliquet bibendum enim. Convallis convallis tellus id interdum. Aliquam ultrices sagittis orci a scelerisque purus semper. 
+    Morbi tristique senectus et netus et malesuada fames ac turpis. Nam at lectus urna duis convallis convallis tellus id interdum. 
+    Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Urna molestie at elementum eu. Sit amet nulla facilisi morbi tempus iaculis urna id. 
+    Amet consectetur adipiscing elit pellentesque habitant. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis. \\n\\n
+    Lacus sed turpis tincidunt id aliquet risus feugiat in. Integer eget aliquet nibh praesent. Ac turpis egestas integer eget aliquet. 
+    Egestas diam in arcu cursus euismod quis viverra nibh.`
   }
 };
